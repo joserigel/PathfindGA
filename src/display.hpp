ae659ea29229cfacf9baa3dc52ef677f4869e77f
joserigel <joserigel18@gmail.com>
2025-12-16 10:45:07 +0100

    Initialized SDL3 Project

---
+++ src/display.hpp
@@ -0,0 +1,12 @@
+#ifndef __DISPLAY_HPP__
+#define __DISPLAY_HPP__
+
+#include <SDL3/SDL.h>
+
+class Display {
+  private:
+  public:
+
+};
+
+#endif /* __DRAW_HPP__ */
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+
+using namespace std;
+
+int main() {
+  cout << "hello world" << endl;
+  return 0;
+}
